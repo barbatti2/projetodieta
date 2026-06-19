fe9e9585139636a1d2b42997debce6bd72ba53e7
barbatti2 <gabriel_barbatti@hotmail.com>
2026-06-19 08:09:20 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI_A_API_KEY_EXATA_DO_FIREBASE",
+  apiKey: "AIzaSyBi72j6Gc574v0oQVe8IFzQgBJN3rRVFWw",
   authDomain: "projetodieta-10.firebaseapp.com",
   projectId: "projetodieta-10",
   storageBucket: "projetodieta-10.firebasestorage.app",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId: "1:231336204822:web:4103b58d33549bea0b3337"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
